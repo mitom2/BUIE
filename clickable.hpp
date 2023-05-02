a3c0e49d46c10d272829d4f66559d272737c7a1e
mitom2 <mitom2@op.pl>
2023-05-02 11:25:46 +0200

    Create empty Clickable class

---
+++ clickable.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include "SFML/System.hpp"
+#include "SFML/Window.hpp"
+#include "SFML/Graphics.hpp"
+
+namespace buie
+{
+
+	class Clickable : public sf::RectangleShape
+	{
+
+	};
+
+}
